08989b69ea6e8c06b77e3fdbdef4e01b4890897b
younglord12 <sergeymoiseenko90@mail.ru>
2025-09-08 15:34:35 +0300

    commit 4les

---
+++ src/lesson4.kt
@@ -0,0 +1,27 @@
+fun main() { //first task
+    val intArray = intArrayOf(1, 2, 3, 4, 5)
+    var max = intArray[0]
+    var maxIndex = 0
+    var min = intArray[0]
+    var minIndex = 0
+
+    for (i in intArray.indices) {
+        if (intArray[i] > max) {
+            max = intArray[i]
+            maxIndex = i
+        }
+    }
+
+
+    for (i in intArray.indices) {
+        if (intArray[i] < min) {
+            min = intArray[i]
+            minIndex = i
+        }
+    }
+
+    println("Получилось максимальное число: $max")
+    println("Позиция максимального числа: $maxIndex")
+    println("Получилось минимальное число: $min")
+    println("Позиция максимального числа: $minIndex")
+}
