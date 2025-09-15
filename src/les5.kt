b9f8c1be1757d5964f8766605cd8952202d391e6
younglord12 <sergeymoiseenko90@mail.ru>
2025-09-15 20:14:47 +0300

    commit 5les 1task

---
+++ src/les5.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val str: String = "happy ovechkin neymar"
+    val builder = StringBuilder()
+    builder.append(str)
+    println(builder.toString())
+    val enough = str.split(" ").reversed().joinToString(" ")
+    println(enough)
+}
